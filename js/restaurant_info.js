bf7f7d9cff0e1d724eaebcd1cd00dc89415a231a
Mark Simpson <markyz1989@gmail.com>
2018-06-21 17:50:22 +0100

    Initial commit - added API key to two files

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -22,7 +22,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
       L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-        mapboxToken: '<your MAPBOX API KEY HERE>',
+        mapboxToken: 'pk.eyJ1IjoibWFya3l6ODkiLCJhIjoiY2ppb2J6ZmVuMGFyYjN2cW1kcno3djJ2eSJ9.wXb1cstFiI-F9QNUuhNQOw',
         maxZoom: 18,
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
           '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
